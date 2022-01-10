56ad754bcd48a85ee11c8480e70c81de0f821a85
akerg <91136312+akerg@users.noreply.github.com>
2022-01-10 12:25:05 +0500

    Add files via upload

---
+++ Pharmacy/IRepositories/IMedicineRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pharmacy.Models;
+
+namespace Pharmacy.IRepositories
+{
+    internal interface IMedicineRepository
+    {
+        public void AddMedicine(Medicine medicine);
+        public void DeleteMedicine(int id);
+        public void UpdateMedicine(int id, Medicine updatedMedicine);
+        public void ShowAll();
+        public Medicine? Find(string keyword);
+    }
+}
---
+++ Pharmacy/Models/Medicine.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Pharmacy.Models
+{
+    public class Medicine
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("Name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+        public void Show()
+        {
+            Console.WriteLine("+-----+--------------------+---------------+\n" +
+                $"|{this.ID,-5}|{this.Name,-20}|{this.Price,-15}|" +
+                "\n+-----+--------------------+---------------+");
+        }
+    }
+}
